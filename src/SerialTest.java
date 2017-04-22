1939c4527c856bc9969ebccd6c295bc54b1d9004
Troels Just Christoffersen <troelsjc@gmail.com>
2017-04-22 21:57:25 +0200

    changed log.csv file path to root relative

--- src/SerialTest.java
+++ src/SerialTest.java
@@ -89,7 +89,8 @@ public class SerialTest implements SerialPortEventListener {
 					if (inputLine.equals("loserville")) {
 						System.out.println("Starting sensor sampling");
 						//File fin = new File("/tmp/log.csv"); // a linux example
-						File fin = new File("C:\\Users\\tjc\\git\\DTU\\NP-HA-SENSIM\\log.csv");
+//						File fin = new File("C:\\Users\\troels\\git\\DTU\\NP-HA-SENSIM\\log.csv");
+						File fin = new File("log.csv");
 						try {
 							ArrayList<String> outputList = readFile(fin);
 
